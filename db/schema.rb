abd23e0164834712dcc701fadd70aa75e7118f51
Craig Petterson <cspetterson@hey.com>
2024-09-08 16:54:26 +0100

    Add relations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_08_154852) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_08_155115) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_08_154852) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_quotes_on_company_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,8 +36,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_08_154852) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "quotes", "companies"
+  add_foreign_key "users", "companies"
 end
