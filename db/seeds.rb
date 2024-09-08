a0c51e9b4c6526636995acefbbdba9d905761062
Craig Petterson <cspetterson@hey.com>
2024-09-08 18:09:48 +0100

    Add db:fixtures:load to seeds (this was done in Chapter 1 or something)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "\n== Seeding the database with fixtures =="
+system("bin/rails db:fixtures:load")
