3a1e0396b34730e1a7ded5fd3be42e3264da6135
Craig Petterson <cspetterson@hey.com>
2024-09-08 18:57:46 +0100

    Line item actions without Turbo

---
+++ app/controllers/line_items_controller.rb
@@ -0,0 +1,54 @@
+class LineItemsController < ApplicationController
+  before_action :set_quote
+  before_action :set_line_item_date
+  before_action :set_line_item, only: [:edit, :update, :destroy]
+
+  def new
+    @line_item = @line_item_date.line_items.build
+  end
+
+  def create
+    @line_item = @line_item_date.line_items.build(line_item_params)
+
+    if @line_item.save
+      redirect_to quote_path(@quote), notice: "Item was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @line_item.update(line_item_params)
+      redirect_to quote_path(@quote), notice: "Item was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @line_item.destroy
+
+    redirect_to quote_path(@quote), notice: "Item was successfully destroyed."
+  end
+
+  private
+
+  def line_item_params
+    params.require(:line_item).permit(:name, :description, :quantity, :unit_price)
+  end
+
+  def set_quote
+    @quote = current_company.quotes.find(params[:quote_id])
+  end
+
+  def set_line_item_date
+    @line_item_date = @quote.line_item_dates.find(params[:line_item_date_id])
+  end
+
+  def set_line_item
+    @line_item = @line_item_date.line_items.find(params[:id])
+  end
+end
