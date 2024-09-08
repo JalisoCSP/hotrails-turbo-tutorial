a16feb4863a1502a1e9ef6a6dd471bad5c0170f8
Craig Petterson <cspetterson@hey.com>
2024-09-08 18:52:21 +0100

    Line item styles

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
 end
