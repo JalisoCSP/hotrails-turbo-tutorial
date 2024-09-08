f943678c93da9e857c3a005c054006b735a93788
Craig Petterson <cspetterson@hey.com>
2024-09-08 19:43:45 +0100

    Turbo total price

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,10 +2,15 @@ class Quote < ApplicationRecord
   belongs_to :company
 
   has_many :line_item_dates, dependent: :destroy
+  has_many :line_items, through: :line_item_dates
 
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
 
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
