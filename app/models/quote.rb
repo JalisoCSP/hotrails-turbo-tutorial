a3ebb153ae52af21c16fcf8cdc9c0764d3bcdb7a
Craig Petterson <cspetterson@hey.com>
2024-09-08 18:16:42 +0100

    Add line item dates to quote show

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,8 @@
 class Quote < ApplicationRecord
   belongs_to :company
 
+  has_many :line_item_dates, dependent: :destroy
+
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
