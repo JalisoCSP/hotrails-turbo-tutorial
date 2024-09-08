6df93c960a7f620344028facd8d27ea9b0de4a0b
Craig Petterson <cspetterson@hey.com>
2024-09-08 18:33:03 +0100

    Turbo line items

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -24,7 +24,10 @@ class LineItemDatesController < ApplicationController
 
   def update
     if @line_item_date.update(line_item_date_params)
-      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Date was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Date was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -33,7 +36,10 @@ class LineItemDatesController < ApplicationController
   def destroy
     @line_item_date.destroy
 
-    redirect_to quote_path(@quote), notice: "Date was successfully destroyed."
+    respond_to do |format|
+      format.html { redirect_to quote_path(@quote), notice: "Date was successfully destroyed." }
+      format.turbo_stream { flash.now[:notice] = "Date was successfully destroyed." }
+    end
   end
 
   private
