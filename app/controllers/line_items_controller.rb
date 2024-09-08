9e3c2714ffb3142c7efd1b089b54952b4a70573e
Craig Petterson <cspetterson@hey.com>
2024-09-08 19:23:35 +0100

    Nested turbo frames

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -11,7 +11,10 @@ class LineItemsController < ApplicationController
     @line_item = @line_item_date.line_items.build(line_item_params)
 
     if @line_item.save
-      redirect_to quote_path(@quote), notice: "Item was successfully created."
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Item was successfully created." }
+        format.turbo_stream { flash.now[:notice] = "Item was successfully created." }
+      end
     else
       render :new, status: :unprocessable_entity
     end
@@ -22,7 +25,10 @@ class LineItemsController < ApplicationController
 
   def update
     if @line_item.update(line_item_params)
-      redirect_to quote_path(@quote), notice: "Item was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Item was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Item was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
@@ -31,7 +37,10 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
 
-    redirect_to quote_path(@quote), notice: "Item was successfully destroyed."
+    respond_to do |format|
+      format.html { redirect_to quote_path(@quote), notice: "Date was successfully destroyed." }
+      format.turbo_stream { flash.now[:notice] = "Date was successfully destroyed." }
+    end
   end
 
   private
